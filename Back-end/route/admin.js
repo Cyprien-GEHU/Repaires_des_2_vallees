9cca5449ffe636caae72236b3e8f91d3cf64424f
Cyprien <cypriengehu@example.com>
2025-06-13 15:41:18 +0200

    update: add the module middleware of authToken in route

--- Back-end/route/admin.js
+++ Back-end/route/admin.js
@@ -3,34 +3,35 @@ const userControl = require('../controllers/user')
 const articleControl = require('../controllers/article')
 const agendaControl = require('../controllers/agenda')
 const eventControl = require('../controllers/event')
+const authToken = require('../middleware/authToken')
 const router = express.Router();
 
 //Router GET
-router.get('/user', userControl.getAllUser);
-router.get('/user/:id', userControl.getOneUser);
-router.get('/article', articleControl.get_article);
-router.get('/article/:id', articleControl.get_OneArticle);
-router.get('/agenda', agendaControl.get_agenda)
-router.get('/agenda/:id', agendaControl.get_OneAgenda);
-router.get('/event', eventControl.get_event);
-router.get('/event/:id', eventControl.get_OneEvent);
+router.get('/user', authToken, userControl.getAllUser);
+router.get('/user/:id', authToken, userControl.getOneUser);
+router.get('/article', authToken, articleControl.get_article);
+router.get('/article/:id', authToken, articleControl.get_OneArticle);
+router.get('/agenda', authToken, agendaControl.get_agenda)
+router.get('/agenda/:id', authToken, agendaControl.get_OneAgenda);
+router.get('/event', authToken, eventControl.get_event);
+router.get('/event/:id', authToken, eventControl.get_OneEvent);
 
 //Router POST
-router.post('/user', userControl.create_user)
-router.post('/article', articleControl.create_article);
-router.post('/agenda', agendaControl.create_agenda);
-router.post('/event', eventControl.create_event);
+router.post('/user', authToken, userControl.create_user)
+router.post('/article', authToken, articleControl.create_article);
+router.post('/agenda', authToken, agendaControl.create_agenda);
+router.post('/event', authToken, eventControl.create_event);
 
 //Router PUT
-router.put('/user/:id', userControl.update_user);
-router.put('/article/:id', articleControl.update_article);
-router.put('/agenda/:id', agendaControl.update_agenda);
-router.put('/event/:id', eventControl.update_event);
+router.put('/user/:id', authToken, userControl.update_user);
+router.put('/article/:id', authToken, articleControl.update_article);
+router.put('/agenda/:id', authToken, agendaControl.update_agenda);
+router.put('/event/:id', authToken, eventControl.update_event);
 
 //Router DELETE
-router.delete('/user/:id', userControl.delete_user);
-router.delete('/article/:id', articleControl.delete_article);
-router.delete('/agenda/:id', agendaControl.delete_agenda);
-router.delete('/event/:id', eventControl.delete_event);
+router.delete('/user/:id', authToken, userControl.delete_user);
+router.delete('/article/:id', authToken, articleControl.delete_article);
+router.delete('/agenda/:id', authToken, agendaControl.delete_agenda);
+router.delete('/event/:id', authToken, eventControl.delete_event);
 
 module.exports = router;
